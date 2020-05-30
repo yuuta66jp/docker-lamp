c7d960e8ab3ccf080a3d65b6122d1c3c91bdca4f
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-05-30 18:18:45 +0900

    [finish]注文フォームチェック画面

---
+++ html/shop/shop_form_check.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+
+require_once('../common/common.php');
+
+$post = sanitize($_POST);
+
+$onamae = $post['onamae'];
+$email = $post['email'];
+$postal1 = $post['postal1'];
+$postal2 = $post['postal2'];
+$address = $post['address'];
+$tel = $post['tel'];
+
+$okflg = true;
+
+if ($onamae == '') {
+  print 'お名前が入力されていません。<br /><br />';
+  $okflg = false;
+} else {
+  print 'お名前<br />';
+  print $onamae;
+  print '<br /><br />';
+}
+// 正規表現(メールアドレスのチェック)
+if (preg_match('/\A[\w\-\.]+\@[\w\-\.]+.([a-z]+)\z/', $email) == 0) {
+  print 'メールアドレスを正確に入力してください。<br /><br />';
+  $okflg = false;
+} else {
+  print 'メールアドレス<br />';
+  print $email;
+  print '<br /><br />';
+}
+// 正規表現(半角数字のチェック)
+if (preg_match('/\A[0-9]+\z/', $postal1) == 0) {
+  print '郵便番号は半角数字で入力してください。<br /><br />';
+  $okflg = false;
+} else {
+  print '郵便番号<br />';
+  print $postal1;
+  print '-';
+  print $postal2;
+  print '<br /><br />';
+}
+// 正規表現(半角数字のチェック)
+if (preg_match('/\A[0-9]+\z/', $postal2) == 0) {
+  print '郵便番号は半角数字で入力してください。<br /><br />';
+  $okflg = false;
+}
+
+if ($address == '') {
+  print '住所が入力されていません。<br /><br />';
+  $okflg = false;
+} else {
+  print '住所<br />';
+  print $address;
+  print '<br /><br />';
+}
+// 正規表現(電話番号のチェック)
+if (preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/', $tel) == 0) {
+  print '電話番号を正確に入力してください。<br /><br />';
+  $okflg = false;
+} else {
+  print '電話番号<br />';
+  print $tel;
+  print '<br /><br />';
+}
+
+if ($okflg == true) {
+  print '<form method="post" action="shop_done.php">';
+  print '<input type="hidden" name="onamae" value="'.$onamae.'">';
+  print '<input type="hidden" name="email" value="'.$email.'">';
+  print '<input type="hidden" name="postl1" value="'.$postl1.'">';
+  print '<input type="hidden" name="postl2" value="'.$postl2.'">';
+  print '<input type="hidden" name="address" value="'.$address.'">';
+  print '<input type="hidden" name="tel" value="'.$tel.'">';
+  print '<input type="button" onclick="history.back()" value="戻る">';
+  print '<input type="submit" value="OK"><br />';
+  print '</form>';
+} else {
+  print '<form>';
+  print '<input type="button" onclick="history.back()" value="戻る">';
+  print '</form>';
+
+}
+
+?>
+ 
+</body>
+</html>
