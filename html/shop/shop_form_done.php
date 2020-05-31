60f97aae65b0757de694beb5a2a1b8c7016274e1
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-05-31 18:29:46 +0900

    [finish]注文情報をデータベースに保存する

--- html/shop/shop_form_done.php
+++ html/shop/shop_form_done.php
@@ -60,6 +60,7 @@ try {
 
     $name = $rec['name'];
     $price = $rec['price'];
+    $kakaku[] = $price;
     $suryo = $kazu[$i];
     $shokei = $price * $suryo;
 
@@ -68,6 +69,46 @@ try {
     $honbun .= $suryo.'個＝';
     $honbun .= $shokei."円\n";
   }
+
+  // テーブルにロックをかけ安全に処理する
+  $sql = 'LOCK TABLES dat_sales WRITE, dat_sales_product WRITE';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  $sql = 'INSERT INTO dat_sales (code_member, name, email, postal1, postal2, address, tel) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  $stmt = $dbh->prepare($sql);
+  // array()で配列の初期化を行う
+  $data = array();
+  $data[] = 0;
+  $data[] = $onamae;
+  $data[] = $email;
+  $data[] = $postal1;
+  $data[] = $postal2;
+  $data[] = $address;
+  $data[] = $tel;
+  $stmt->execute($data);
+
+  // SELECT LAST_INSERT_ID()で最後にINSERTされたIDを取得する
+  $sql = 'SELECT LAST_INSERT_ID()';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+  $lastcode = $rec['LAST_INSERT_ID()'];
+
+  for ($i = 0; $i < $max; $i++) {
+    $sql = 'INSERT INTO dat_sales_product (code_sales, code_product, price, quantity) VALUES (?, ?, ?, ?)';
+    $stmt = $dbh->prepare($sql);
+    $data = array();
+    $data[] = $lastcode;
+    $data[] = $cart[$i];
+    $data[] = $kakaku[$i];
+    $data[] = $kazu[$i];
+    $stmt->execute($data);
+  }
+
+  $sql = 'UNLOCK TABLES';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
 
   $dbh = null;
 
@@ -113,6 +154,9 @@ try {
 }
 
 ?>
+
+<br />
+<a href="shop_list.php">商品画面へ</a>
  
 </body>
 </html>
