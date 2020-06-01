5f33584c5ec368631697c89f3bab336f8ad7adb8
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-06-01 12:14:59 +0900

    [finish]注文データダウンロード画面

--- html/order/order_download.php
+++ html/order/order_download.php
@@ -22,15 +22,21 @@ if (isset ($_SESSION['login']) == false) {
 </head>
 <body>
 
-ショップ管理トップメニュー<br />
-<br />
-<a href="../staff/staff_list.php">スタッフ管理</a><br />
-<br />
-<a href="../product/pro_list.php">商品管理</a><br />
-<br />
-<a href="../order/order_download.php">注文ダウンロード</a><br />
-<br />
-<a href="staff_logout.php">ログアウト</a><br />
+<?php
+require_once('../common/common.php');
+?>
+
+ダウンロードしたい注文日を選んでください。<br />
+<form method="post" action="order_download_done.php">
+  <?php pulldown_year(); ?>
+  年
+  <?php pulldown_month(); ?>
+  月
+  <?php pulldown_day(); ?>
+  日<br />
+  <br />
+  <input type="submit" value="ダウンロードへ">
+</form>
 
 </body>
 </html>
